862132d82147fe4012671276f7ec61ac22e23f4f
shmsh9 <virgile.carbonari@gmail.com>
2022-06-22 20:49:15 +0200

    hummm...

--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -1,8 +1,8 @@
 #ifndef KERNEL_H_
 #define KERNEL_H_
 #include <efi.h>
-#include <serial.h>
 #include <kstd.h>
+#include <drivers/serial.h>
 #include <bootloader.h>
 #endif
 
--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,7 +1,21 @@
 #include <kernel.h>
-#include <drivers/serial.h>
 
 uint64_t kmain(struct bootinfo *bootinfo){
+	SERIAL_PUTCHAR('H');
+	SERIAL_PUTCHAR('E');
+	SERIAL_PUTCHAR('L');
+	SERIAL_PUTCHAR('L');
+	SERIAL_PUTCHAR('L');
+	SERIAL_PUTCHAR('O');
+	SERIAL_PUTCHAR(' ');
+	SERIAL_PUTCHAR('W');
+	SERIAL_PUTCHAR('O');
+	SERIAL_PUTCHAR('R');
+	SERIAL_PUTCHAR('L');
+	SERIAL_PUTCHAR('D');
+	SERIAL_PUTCHAR('!');
+	SERIAL_PUTCHAR('\n');
+	//Stack problems on aarch64
 	kprint("Hello from l00n1x\n");
 	kprint("Second kprint()\n");
 	kprintf("kbase    : 0x%x\nksize    : 0x%x\n", bootinfo->kernelbase, bootinfo->kernelsize);
