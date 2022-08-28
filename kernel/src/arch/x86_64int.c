766ec0b780f1e9ca5bc655fbd26494ddbdfd8d3c
shmsh9 <virgile.carbonari@gmail.com>
2022-08-28 18:24:34 +0200

    fix interrupt gate offseted

--- kernel/src/arch/x86_64int.c
+++ kernel/src/arch/x86_64int.c
@@ -4,9 +4,8 @@
 #ifdef __x86_64__
 
 __attribute__((aligned(0x10))) static idt_entry_t idt[256] = {0}; // Create an array of IDT entries; aligned for performance
-__attribute__((aligned(0x10))) uint64_t idt_stub_table[256] = {0};
 static idtr_t idtr;
-extern uint64_t asm_interrupt_table[32];
+extern uint64_t asm_interrupt_table[256];
 
 void exception_handler(x86_64_interrupt_frame frame, uint64_t rdi_interrupt_num){
     KDEBUG("\nexception (%d) :\n\t[rip]    : 0x%x\n\t[rsp]    : 0x%x\n\t[rflags] : 0b%b\n\t[cs]     : 0x%x\n\t[ss]     : 0x%x", 
@@ -21,23 +20,23 @@ void exception_handler(x86_64_interrupt_frame frame, uint64_t rdi_interrupt_num)
 void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags){
     idt_entry_t* descriptor = &idt[vector];
  
-    descriptor->isr_low       = ((uint64_t)isr+vector) & 0xFFFF;
+    descriptor->isr_low       = ((uint64_t)isr) & 0xFFFF;
     descriptor->kernel_cs     = 0x8; //kernel offset in gdt
     descriptor->ist           = 0;
     descriptor->attributes    = flags;
-    descriptor->isr_mid       = (((uint64_t)isr+vector) >> 16) & 0xFFFF;
-    descriptor->isr_high      = (((uint64_t)isr+vector) >> 32) & 0xFFFFFFFF;
+    descriptor->isr_mid       = (((uint64_t)isr) >> 16) & 0xFFFF;
+    descriptor->isr_high      = (((uint64_t)isr) >> 32) & 0xFFFFFFFF;
     descriptor->reserved      = 0;
 }
 void idt_init(bootinfo *bi){
-    KDEBUG("asm_interrupt_table 0x%x", asm_interrupt_table);
-    KDEBUG("asm_interrupt_table[0] 0x%x", asm_interrupt_table[0]+(uint64_t)bi->kernelbase);
-    for(uint16_t i = 0; i < 256; i++)
-        idt_stub_table[i] = (uint64_t)asm_interrupt_table[i]+(uint64_t)bi->kernelbase;
+
     idtr.base = (uintptr_t)&idt[0];
-    idtr.limit = (uint16_t)sizeof(idt_entry_t) * 32 - 1;
-    for (uint8_t vector = 0; vector < 32; vector++) {
-        idt_set_descriptor(vector, (void *)idt_stub_table[vector], 0x8E);
+    idtr.limit = (uint16_t)sizeof(idt_entry_t) * 256 - 1;
+    for(uint16_t vector = 0; vector < 256; vector++){
+        idt_set_descriptor(vector,
+            (void *)((uint64_t)asm_interrupt_table[vector]+(uint64_t)bi->kernelbase), 
+            0x8E
+        );
     }
     __asm__ volatile ("lidt %0" : : "m"(idtr)); // load the new IDT
     __asm__ volatile ("sti"); // set the interrupt flag
