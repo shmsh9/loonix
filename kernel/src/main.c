94791c719b83d393c609cb9fe4f6616e6a7291cd
Carbonari <Virgile.Carbonari@magna.com>
2022-09-27 13:13:57 +0200

    move to timer

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -8,8 +8,11 @@ uint64_t kmain(bootinfo *bootinfo){
 	KMESSAGE("Framebuffer %dx%d at 0x%x (%d MB)", fb->width, fb->height, fb->buffer, BYTES_TO_MB(fb->size));
 	KMESSAGE("Available system memory %d MB", BYTES_TO_MB(heap.free_memory));
 	kprint("Welcome to l00n1x !\n");
+	timer_event_new(
+		(void (*)(void *))framebuffer_device_update,
+		(void *)fb
+	);
 	while(1){
-		vt100_console_update_draw_screen(fb);
 		shell_non_blocking();
 		/* we cannot return since we switched the stack */
 	}
