089d052033c8a71bcd160b51e5b982e119eca214
Virgile Carbonari <Virgile.Carbonari@mmt-b.com>
2024-12-18 15:56:23 +0100

    structs fs

--- kernel/src/fs/fs.c
+++ kernel/src/fs/fs.c
@@ -1,7 +1,7 @@
 #include <fs/fs.h>
 
 bool is_fs(uint8_t *b){
-    return ((fs_md *)b)->magic == FS_MAGIC;
+    return ((fs_md *)b)->magic == FS_MAGIC && ((fs_md *)b)->block_sz == FS_BLOCK_SZ;
 }
 
 bool set_block_dirty(fs_md *fs, uint64_t b){
