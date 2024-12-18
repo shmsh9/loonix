483712ca6fdac4a6e57513029fb83b450d95b7b8
shmsh9 <virgile.carbonari@gmail.com>
2024-12-18 17:34:13 +0100

    fs_commit_bitmap()

--- kernel/src/fs/fs.c
+++ kernel/src/fs/fs.c
@@ -4,11 +4,19 @@ bool is_fs(uint8_t *b){
     return ((fs_md *)b)->magic == FS_MAGIC && ((fs_md *)b)->block_sz == FS_BLOCK_SZ;
 }
 
+bool fs_commit_bitmap(fs_md *fs){
+    task_lock();
+
+    task_unlock();
+    return true;
+}
+
 bool set_block_dirty(fs_md *fs, uint64_t b){
     if(b >= fs->block_n)
         return false;
     task_lock();
     set_bit(fs->bitmap_blocks+(b/FS_BITMAP_SZ),  b%FS_BITMAP_SZ);
+    fs_commit_bitmap(fs);
     task_unlock();
     return true;
 }
@@ -17,6 +25,7 @@ bool set_block_clean(fs_md *fs, uint64_t b){
         return false;
     task_lock();
     unset_bit(fs->bitmap_blocks+(b/FS_BITMAP_SZ),  b%FS_BITMAP_SZ);
+    fs_commit_bitmap(fs);
     task_unlock();
     return true;
 }
