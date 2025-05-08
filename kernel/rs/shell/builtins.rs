eeecfaf8088b940c8200ff76c8056e5dfb36237f
shmsh9 <virgile.carbonari@gmail.com>
2025-05-08 17:38:00 +0200

    more rs work

--- kernel/rs/shell/builtins.rs
+++ kernel/rs/shell/builtins.rs
@@ -1,13 +1,18 @@
 
 use alloc::string::ToString;
 use String;
-const BUILTINS : [Builtin; 0] = [];
+const BUILTINS : [Builtin; 0] = [
 
+];
+
+extern "C" help(argc: i32, argv: *const *const u8) -> i32{
+    
+}
 #[derive(Debug)]
 #[repr(C)]
 struct Builtin{
     name: String,
-    function: *const extern "C" fn(argc: i32, argv: *const *const u8)
+    function: *const extern "C" fn(argc: i32, argv: *const *const u8) -> i32
 }
 impl Builtin{
     fn new(n: &str, f : *const extern "C" fn(i32, *const *const u8)) -> Builtin {
