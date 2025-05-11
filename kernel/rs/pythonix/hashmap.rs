4a0a49ec1835707ad5588263778c4e6abba4c46a
shmsh9 <virgile.carbonari@gmail.com>
2025-05-11 11:37:55 +0200

    HashMap<T>

--- kernel/rs/pythonix/hashmap.rs
+++ kernel/rs/pythonix/hashmap.rs
@@ -1,59 +1,59 @@
 use alloc::boxed::Box;
 use interpreter::PyType;
 #[derive(Debug)]
-pub struct HashMap<'a>{
-    root: Option<Node<'a>>
+pub struct HashMap<'a,T>{
+    root: Option<Node<'a, T>>
 }
-impl<'a> HashMap<'a>{
+fn hash(k: &PyType) -> u64{
+    match k{
+        PyType::str(s) => {
+            let mut h : [u8;8] = [0;8];
+            let mut j = 0;
+            for i in 0..s.len(){
+                if j == 8{
+                    j = 0;
+                }
+                h[j] ^= s.chars().nth(i).unwrap() as u8 + i as u8;
+                j += 1;
+            }
+            return u64::from_le_bytes(h);
+        },
+        PyType::int(i) => return *i as u64,
+        _ => panic!("cannot hash value {:?}", k)
+    }
+}
+impl<'a,T> HashMap<'a,T>{
     pub fn new() -> Self{
         Self{
             root: None
         }
     }
-    pub fn get(&self, k: &PyType) -> Option<&PyType>{
-        let h = HashMap::hash(k);
+    pub fn get(&self, k: &PyType) -> Option<&T>{
+        let h = hash(k);
         match &self.root{
             Some(n) => return n.get(h),
             None => return None
         }
     }
-    pub fn insert(&mut self, k: &PyType, v: &'a PyType){
-        let h = HashMap::hash(k);
+    pub fn insert(&mut self, k: &PyType, v: &'a T){
+        let h = hash(k);
         match &mut self.root{
             None => self.root = Some(Node::new(h,v)),
             Some(n) => n.insert(h,v)
         }   
     } 
-    fn hash(k: &PyType) -> u64{
-        match k{
-            PyType::str(s) => {
-                let mut h : [u8;8] = [0;8];
-                let mut j = 0;
-                for i in 0..s.len(){
-                    if j == 8{
-                        j = 0;
-                    }
-                    h[j] ^= s.chars().nth(i).unwrap() as u8 + i as u8;
-                    j += 1;
-                }
-                return u64::from_le_bytes(h);
-            },
-            PyType::int(i) => return *i as u64,
-            _ => panic!("cannot hash value {:?}", k)
-        }
-    }
 }
 
 #[derive(Debug)]
-struct Node<'a>{
+struct Node<'a, T>{
     key: u64,
-    value: &'a PyType,
-    left: Option<Box<Node<'a>>>,
-    right: Option<Box<Node<'a>>> 
+    value: &'a T,
+    left: Option<Box<Node<'a,T>>>,
+    right: Option<Box<Node<'a,T>>> 
 }
 
-impl<'a> Node<'a>{
-    fn new(k: u64, v: &'a PyType) -> Node<'a>{
+impl<'a,T> Node<'a,T>{
+    fn new(k: u64, v: &'a T) -> Node<'a,T>{
         return Node{
             key: k,
             value: v,
@@ -61,7 +61,7 @@ impl<'a> Node<'a>{
             right: None
         }
     }
-    fn insert(&mut self, k: u64, v: &'a PyType){
+    fn insert(&mut self, k: u64, v: &'a T){
         if k == self.key {
             self.value = v
         }
@@ -78,7 +78,7 @@ impl<'a> Node<'a>{
             }
         }
     }
-    fn get(&self, k: u64) -> Option<&PyType>{
+    fn get(&self, k: u64) -> Option<&T>{
         if k == self.key{
             return Some(&self.value);
         }
